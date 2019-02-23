7c036db9fc699063d57b1ba6e566b591a58ee26f
Harry Ross <hgr8@duke.edu>
2019-02-23 17:48:06 -0500

    Added if/else commands, light implementation of Make, Repeat

---
+++ src/parser/commands/control_commands/MakeVariableCommand.java
@@ -0,0 +1,19 @@
+package parser.commands.control_commands;
+
+import parser.Command;
+import parser.EvalCommand;
+import java.util.List;
+
+public class MakeVariableCommand extends EvalCommand {
+
+    private Command myContents;
+
+    public MakeVariableCommand(List<Command> params) {
+        myContents = params.get(0);
+    }
+
+    @Override
+    public double execute() {
+        return myContents.execute();
+    }
+}
---
+++ src/parser/commands/control_commands/RepeatCommand.java
@@ -0,0 +1,27 @@
+package parser.commands.control_commands;
+
+import parser.Command;
+import parser.EvalCommand;
+
+import java.util.List;
+
+public class RepeatCommand extends EvalCommand {
+
+    private Command myTotalIter;
+    private Command myList;
+    private Command myCurrentIter; // :repcount
+
+    public RepeatCommand(List<Command> params) {
+        myTotalIter = params.get(0);
+        myList = params.get(1);
+        myCurrentIter = null; // TODO call to parser to store, update :repcount as user defined variable but make sure to save/load previous value
+    }
+
+    @Override
+    public double execute() {
+        for (int i = 0; i < myTotalIter.execute() - 1; i++) {
+            myList.execute();
+        }
+        return myList.execute(); // Final iteration
+    }
+}
