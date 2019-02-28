02dc033c930e23c95122579fb628b5f5091304e0
Carter Gay <cag65@duke.edu>
2019-02-28 02:02:06 -0500

    exception

--- frontend/src/ui_private/displays/CommandTerminal.java
+++ frontend/src/ui_private/displays/CommandTerminal.java
@@ -1,21 +1,30 @@
 package ui_private.displays;
 
+import javafx.scene.Node;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
 import parser_public.CommandParser;
 import parser_public.ParserException;
-
+import javafx.event.ActionEvent;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class CommandTerminal {
     public static final double PARSE_BUTTON_HEIGHT = 50;
+    public static final double MAX_WIDTH = 500;
+    public static final double V_GAP = 30;
 
+    private GridPane myPane;
+    private ArrayList<Node> myChildren;
     private Button myParseButton;
     private TextArea myCommandInput;
     private String myCommand;
 
     public CommandTerminal() {
         myParseButton = new Button("PARSE");
-        myParseButton.setOnAction((event) -> sendToParser());
+        myParseButton.setOnAction((event) -> {
+            sendToParser();
+        });
         myCommandInput = new TextArea();
         myCommandInput.setPrefRowCount(10);
         myCommandInput.setPrefColumnCount(10);
@@ -30,6 +39,7 @@ public class CommandTerminal {
         }
         catch (ParserException e) {
             // TODO handle parse error
+            myCommandInput.setText("Enter a valid command");
         }
     }
 
