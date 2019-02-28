b6bf5e4c9784006fd3c5a2c0548e34a7f3b5f447
Hunter Gregory <hlg16@duke.edu>
2019-02-28 00:01:52 -0500

    added abstract handler to Selector and made Selector a generic

--- frontend/src/ui_private/features/selectors/Selector.java
+++ frontend/src/ui_private/features/selectors/Selector.java
@@ -1,31 +1,44 @@
 package ui_private.features.selectors;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.text.Font;
 import ui_private.features.Feature;
 
-public abstract class Selector extends Feature {
+public abstract class Selector<T> extends Feature {
 
     private static final int NUM_OPTIONS_SHOWN = 4;
 
     public Selector() {
-        ComboBox<?> dropBox = getDropBox();
+        ComboBox<T> dropBox = getDropBox();
         dropBox.setEditable(true);
         dropBox.setVisibleRowCount(NUM_OPTIONS_SHOWN);
         dropBox.setItems(getItemList());
         dropBox.getSelectionModel().selectFirst();
+        T selectedItem = dropBox.getSelectionModel().getSelectedItem();
+        dropBox.setOnAction(handleItemSelected(selectedItem));
     }
-
-
-    abstract protected ObservableList getItemList();
-
 
     /**
      * @return nonnull ComboBox
      */
-    abstract protected ComboBox<?> getDropBox();
+    abstract protected ComboBox<T> getDropBox();
+
+
+    /**
+     * @return List of selectable items
+     */
+    abstract protected ObservableList getItemList();
+
+    /**
+     * Make sure Object is the
+     * @param item
+     * @return
+     */
+    abstract protected EventHandler<ActionEvent> handleItemSelected(T item);
 
 
     @Override
