fe4302cf20565a9e2fe5bcc1d23566f697af64ac
Hunter Gregory <hlg16@duke.edu>
2019-02-28 01:08:08 -0500

    added scrollable window

--- frontend/src/ui_private/features/scrollable_windows/ScrollableWindow.java
+++ frontend/src/ui_private/features/scrollable_windows/ScrollableWindow.java
@@ -1,7 +1,46 @@
 package ui_private.features.scrollable_windows;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import ui_private.features.VerticalFeature;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 public abstract class ScrollableWindow extends VerticalFeature {
 
+    private ScrollPane myScrollPane;
+    private TextArea myTextArea;
+    private LinkedList<String> myTextChain;
+
+    public ScrollableWindow() {
+        myTextChain = new LinkedList<>();
+        myTextArea = new TextArea(getText());
+        myScrollPane = new ScrollPane(myTextArea);
+        myScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+    }
+
+    abstract protected int getMaxLinesDisplayed();
+
+    abstract protected boolean isSortedAlphabetically();
+
+    protected void addText(String newString) {
+        myTextChain.addFirst(newString);
+        if (isSortedAlphabetically())
+            Collections.sort(myTextChain, Comparator.reverseOrder());
+        myTextArea.setText(getText());
+    }
+
+    private String getText() {
+        StringBuilder builder = new StringBuilder();
+        int maxLines = getMaxLinesDisplayed();
+        for (String line : myTextChain) {
+            if (maxLines <= 0)
+                break;
+            builder.append(line + "\n");
+            maxLines -= 1;
+        }
+        return builder.toString();
+    }
 }
