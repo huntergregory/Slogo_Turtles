2010cea7645963be957ea5a4a35b4d94a2f8b7cc
Harry Ross <hgr8@duke.edu>
2019-03-02 18:03:52 -0500

    Terminal now displays specific error messages

--- frontend/src/ui_private/displays/CommandTerminal.java
+++ frontend/src/ui_private/displays/CommandTerminal.java
@@ -40,9 +40,7 @@ public class CommandTerminal {
             CommandParser.getInstance().parseAndRun(myCommand);
         }
         catch (ParserException e) {
-            // TODO handle parse error
-            e.printStackTrace();
-            myCommandInput.setPromptText("Enter a valid command");
+            myCommandInput.setPromptText("--" + e.getMessage() + "-- Enter a valid command");
         }
     }
 
