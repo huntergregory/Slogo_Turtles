7a3db16f54053cc717edab143ad2ea2c0621d7fe
David Miron <david.miron@duke.edu>
2019-03-01 22:22:01 -0500

    Start implementing StateManager

--- state_manager/src/state_public/StateManager.java
+++ state_manager/src/state_public/StateManager.java
@@ -1,4 +1,11 @@
 package state_public;
 
 public class StateManager {
+
+    private List<Turtle> myTurtles;
+
+    private GlobalVariables myVariables;
+    private GlobalCommands myCommands;
+    private
+
 }
