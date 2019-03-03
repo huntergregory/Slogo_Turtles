d875cf1838e8a3c93ec98578ed10d7ab16de7c4f
David Miron <david.miron@duke.edu>
2019-03-02 23:25:00 -0500

    Refactor move distance commands

--- backend/src/parser_private/commands/turtle_commands/move_distance_commands/BackCommand.java
+++ backend/src/parser_private/commands/turtle_commands/move_distance_commands/BackCommand.java
@@ -1,11 +1,12 @@
 package parser_private.commands.turtle_commands.move_distance_commands;
 
 import parser_private.Command;
+import state_public.CommandInter;
 
 import java.util.List;
 
 public class BackCommand extends MoveDistanceCommand {
-    public BackCommand(List<Command> params) {
+    public BackCommand(List<CommandInter> params) {
         super(params, false);
     }
 }
