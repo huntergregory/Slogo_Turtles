39277b311acd4650d27a2912a7c9f368ff355649
David Miron <david.miron@duke.edu>
2019-03-03 00:29:47 -0500

    Refactor visibility commands

--- backend/src/parser_private/commands/turtle_commands/visibility_commands/HideTurtleCommand.java
+++ backend/src/parser_private/commands/turtle_commands/visibility_commands/HideTurtleCommand.java
@@ -1,19 +1,22 @@
 package parser_private.commands.turtle_commands.visibility_commands;
 
 import parser_private.Command;
+import parser_private.commands.turtle_commands.TurtleCommand;
+import state_public.CommandInter;
 
 import java.util.List;
 
 public class HideTurtleCommand extends TurtleCommand {
 
-    public HideTurtleCommand(List<Command> params) {
+    public HideTurtleCommand(List<CommandInter> params) {
         super(params);
     }
 
     @Override
     public double runCommand() {
-        System.out.println("hiding");
-        myManager.setIsShowing(false);
-        return 0;
+        return runTurtleCommand((turtle) -> {
+            turtle.setShowing(false);
+            return 0.0;
+        });
     }
 }
--- backend/src/parser_private/commands/turtle_commands/visibility_commands/ShowTurtleCommand.java
+++ backend/src/parser_private/commands/turtle_commands/visibility_commands/ShowTurtleCommand.java
@@ -1,19 +1,22 @@
 package parser_private.commands.turtle_commands.visibility_commands;
 
 import parser_private.Command;
+import parser_private.commands.turtle_commands.TurtleCommand;
+import state_public.CommandInter;
 
 import java.util.List;
 
 public class ShowTurtleCommand extends TurtleCommand {
 
-    public ShowTurtleCommand(List<Command> params) {
+    public ShowTurtleCommand(List<CommandInter> params) {
         super(params);
     }
 
     @Override
     public double runCommand() {
-        System.out.println("showing");
-        myManager.setIsShowing(true);
-        return 1;
+        return runTurtleCommand((turtle) -> {
+            turtle.setShowing(true);
+            return 1.0;
+        });
     }
 }
