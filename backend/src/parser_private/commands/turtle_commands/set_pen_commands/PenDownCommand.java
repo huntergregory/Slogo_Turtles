b7c9729be4e9444266c0e98335da03c1cc7dfe74
David Miron <david.miron@duke.edu>
2019-03-03 00:26:38 -0500

    Refactor pen commands

--- backend/src/parser_private/commands/turtle_commands/set_pen_commands/PenDownCommand.java
+++ backend/src/parser_private/commands/turtle_commands/set_pen_commands/PenDownCommand.java
@@ -1,18 +1,23 @@
 package parser_private.commands.turtle_commands.set_pen_commands;
 
 import parser_private.Command;
+import parser_private.commands.turtle_commands.TurtleCommand;
+import state_public.CommandInter;
 
 import java.util.List;
 
 public class PenDownCommand extends TurtleCommand {
 
-    public PenDownCommand(List<Command> params) {
+    public PenDownCommand(List<CommandInter> params) {
         super(params);
     }
 
     @Override
     public double runCommand() {
-        myManager.setPenDown(true);
-        return 1;
+        return runTurtleCommand((turtle) -> {
+            turtle.getPen().setIsDown(true);
+            return 1.0;
+        });
+
     }
 }
