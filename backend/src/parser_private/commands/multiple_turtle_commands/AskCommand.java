57c71659a254cab296b60f63ba3e3c10cf4a3228
David Miron <david.miron@duke.edu>
2019-03-05 13:06:19 -0500

    Comment out backend code that does not compile

--- backend/src/parser_private/commands/multiple_turtle_commands/AskCommand.java
+++ backend/src/parser_private/commands/multiple_turtle_commands/AskCommand.java
@@ -1,4 +1,15 @@
 package parser_private.commands.multiple_turtle_commands;
 
-public class AskCommand {
+import parser_private.Command;
+import state_public.CommandInter;
+
+import java.util.List;
+
+public class AskCommand {//extends Command {
+
+    /*public AskCommand(List<CommandInter> params) {
+        super(params);
+
+    }*/
+
 }
