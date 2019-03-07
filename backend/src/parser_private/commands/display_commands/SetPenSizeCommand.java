78e0ba32de0e305c2c8773a2315a4274ac96c35c
David Miron <david.miron@duke.edu>
2019-03-07 12:22:03 -0500

    Implement SetPenSizeCommand

--- backend/src/parser_private/commands/display_commands/SetPenSizeCommand.java
+++ backend/src/parser_private/commands/display_commands/SetPenSizeCommand.java
@@ -1,4 +1,25 @@
 package parser_private.commands.display_commands;
 
-public class SetPenSizeCommand {
+import parser_private.commands.turtle_commands.TurtleCommand;
+import state_public.ICommand;
+
+import java.util.List;
+
+public class SetPenSizeCommand extends TurtleCommand {
+
+    private ICommand myPixels;
+
+    public SetPenSizeCommand(List<ICommand> params) {
+        super(params);
+        myPixels = params.get(0);
+    }
+
+    public double execute() {
+        int pixels = (int)myPixels.execute();
+        runTurtleCommand(turtle -> {
+            turtle.getPen().setThickness(pixels);
+            return 0.0;
+        });
+        return pixels;
+    }
 }
