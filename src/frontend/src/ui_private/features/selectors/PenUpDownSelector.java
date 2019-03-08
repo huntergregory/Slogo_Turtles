9cfdce0157a219b1e5a37ee6fa5966ab9c2341bf
Carter Gay <cag65@duke.edu>
2019-03-08 03:31:07 -0500

    set

--- src/frontend/src/ui_private/features/selectors/PenUpDownSelector.java
+++ src/frontend/src/ui_private/features/selectors/PenUpDownSelector.java
@@ -3,11 +3,12 @@ package ui_private.features.selectors;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import state_public.StateManager;
+import state_public.Turtle;
 import ui_private.displays.CommandTerminal;
 
 public class PenUpDownSelector extends Selector {
     private static final String TITLE = "Pen Up/Down";
-    private  ObservableList PENSTATES;
+    private  ObservableList PENSTATES = FXCollections.observableArrayList("", "UP", "DOWN");
     private String myPenState;
 
     public PenUpDownSelector(StateManager manager) {
@@ -15,13 +16,22 @@ public class PenUpDownSelector extends Selector {
     }
 
     @Override
-    protected void setItemList(ObservableList list) {
-        PENSTATES = FXCollections.observableArrayList("", "UP", "DOWN");
+    protected ObservableList getItemList() {
+        return PENSTATES;
     }
 
     @Override
     protected void handleItemSelected(String item) {
-        myPenState = item;
+        boolean isDown;
+        if (item == "UP") {
+            isDown = true;
+        }
+        else  {
+            isDown = false;
+        }
+        for (Turtle turtle : myStateManager.getTurtleManager().getTurtles()) {
+            turtle.getPen().setIsDown(isDown);
+        }
     }
 
     @Override
