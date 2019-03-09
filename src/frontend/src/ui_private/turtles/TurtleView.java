30d790398e5e3b57fd9eff27c4cb500ac2d7bb53
Hunter Gregory <hlg16@duke.edu>
2019-03-09 02:56:18 -0500

    added animation to turtles. currently bug with pen up and erase lines

--- src/frontend/src/ui_private/turtles/TurtleView.java
+++ src/frontend/src/ui_private/turtles/TurtleView.java
@@ -1,10 +1,17 @@
 package ui_private.turtles;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.Transition;
+import javafx.animation.TranslateTransition;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.util.Duration;
 import state.Turtle;
 
 import java.awt.geom.Point2D;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 /**
@@ -16,6 +23,7 @@ public abstract class TurtleView {
     static final double HEIGHT = 20;
     private static final double INACTIVE_OPACITY = 0.6;
     private static final String CSS_TAG = "turtle";
+    private static final double ANIMATION_LENGTH = 1000;
 
     protected Node myNode; //must be accessed by subclass
     protected Turtle myTurtleStates;
@@ -24,6 +32,7 @@ public abstract class TurtleView {
     private Pen myPen;
     private double myDispXOffset;
     private double myDispYOffset;
+    private Queue<Transition> myTransitions;
 
 
     /**
@@ -39,6 +48,7 @@ public abstract class TurtleView {
         myPen = new Pen(myModifiableList, myTurtleStates.getPen());
         myDispXOffset = dispOffsetX;
         myDispYOffset = dispOffsetY;
+        myTransitions = new LinkedList<>();
 
         initializeNode();
         myNode.setOnMouseClicked(mouseEvent -> toggleActive());
@@ -71,8 +81,14 @@ public abstract class TurtleView {
     }
 
     private void updateOpacity(boolean isActive) {
+        double oldOpacity = (isActive) ? INACTIVE_OPACITY : 1.0;
         double newOpacity = (isActive) ? 1.0 : INACTIVE_OPACITY;
-        myNode.setOpacity(newOpacity);
+
+        var fadeTransition = new FadeTransition();
+        fadeTransition.setFromValue(oldOpacity);
+        fadeTransition.setToValue(newOpacity);
+
+        handleTransition(fadeTransition);
     }
 
     private void bindProperties() {
@@ -81,12 +97,21 @@ public abstract class TurtleView {
 
     private void addPropertyListeners() {
         myTurtleStates.getPositionProperty().addListener((o, oldPosition, newPosition) -> move(oldPosition, newPosition));
-        myTurtleStates.getHeadingProperty().addListener((o, oldHeading, newHeading) -> rotate(newHeading));
+        myTurtleStates.getHeadingProperty().addListener((o, oldHeading, newHeading) -> rotate(oldHeading, newHeading));
         myTurtleStates.getActiveProperty().addListener((o, oldActive, newActive) -> updateOpacity(newActive));
     }
 
     private void move(Point2D oldPoint, Point2D newPoint) {
-        relocateNode(newPoint);
+        var translateTransition = new TranslateTransition();
+        translateTransition.setDuration(Duration.millis(ANIMATION_LENGTH));
+        translateTransition.setNode(myNode);
+        translateTransition.setByX(newPoint.getX() - oldPoint.getX());
+        translateTransition.setByY(newPoint.getY() - oldPoint.getY());
+        translateTransition.setOnFinished(e -> drawLine(oldPoint, newPoint));
+        handleTransition(translateTransition);
+    }
+
+    private void drawLine(Point2D oldPoint, Point2D newPoint) {
         myPen.draw(oldPoint.getX() + myDispXOffset,
                 oldPoint.getY() + myDispYOffset,
                 newPoint.getX() + myDispXOffset,
@@ -94,13 +119,40 @@ public abstract class TurtleView {
         moveAboveLines();
     }
 
+    private void handleTransition(Transition transition) {
+        var oldHandler = transition.getOnFinished();
+        if (oldHandler == null)
+            transition.setOnFinished(e -> removeCurrentAndPlayNext());
+        else
+            transition.setOnFinished(e -> { oldHandler.handle(e); removeCurrentAndPlayNext(); });
+
+        myTransitions.add(transition);
+        if (myTransitions.size() == 1)
+            playNext();
+    }
+
+    private void removeCurrentAndPlayNext() {
+        myTransitions.remove();
+        playNext();
+    }
+
+    private void playNext() {
+        var nextTransition = myTransitions.peek();
+        if (nextTransition != null)
+            nextTransition.play();
+    }
+
     private void relocateNode(Point2D newPoint) {
         myNode.relocate(newPoint.getX() - TurtleView.WIDTH / 2.0 + myDispXOffset,
                 newPoint.getY() - TurtleView.HEIGHT / 2.0 + myDispYOffset);
     }
 
-    private void rotate(Number newVal) {
-        myNode.setRotate(newVal.doubleValue());
+    private void rotate(Number oldAngle, Number newAngle) {
+        var rotateTransition = new RotateTransition();
+        rotateTransition.setDuration(Duration.millis(ANIMATION_LENGTH));
+        rotateTransition.setNode(myNode);
+        rotateTransition.setByAngle(newAngle.doubleValue() - oldAngle.doubleValue());
+        handleTransition(rotateTransition);
     }
 
     /**
