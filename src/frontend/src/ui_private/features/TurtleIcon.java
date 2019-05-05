649f41dbfbd43fa73c09e4c529e1d2593c9cecde
Hunter Gregory <hlg16@duke.edu>
2019-05-05 09:57:55 -0400

    made turtle images feature

---
+++ src/frontend/src/ui_private/features/TurtleIcon.java
@@ -0,0 +1,51 @@
+package ui_private.features;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import state.Turtle;
+import state.TurtleManager;
+
+
+public class TurtleIcon {
+    private static final String TURTLE = "Turtle ";
+
+    private Turtle myTurtle;
+    private ImageView myImageView;
+    private VBox myVBox;
+    private Label myLabel;
+    private ComboBox<String> myComboBox;
+
+    protected TurtleIcon(Turtle turtle) {
+        myTurtle = turtle;
+        myLabel = new Label(TURTLE + turtle.getID());
+        initImageView();
+        initComboBox();
+        myVBox = new VBox(myLabel, myImageView, myComboBox);
+    }
+
+    private void initComboBox() {
+        myComboBox = new ComboBox<>(TurtleManager.TURTLE_IMAGE_FILES);
+        myComboBox.getSelectionModel().select(myTurtle.getImageProperty().get());
+        myComboBox.setEditable(false);
+        myComboBox.valueProperty().addListener((o, old, neww) -> myTurtle.getImageProperty().set(neww));
+    }
+
+    private void initImageView() {
+        var defaultImage = TurtleManager.TURTLE_IMAGE_FILES.get(0);
+        myImageView = new ImageView(getImage(defaultImage));
+        myTurtle.getImageProperty().addListener((o, old, neww) -> myImageView.setImage(getImage(neww)));
+    }
+
+    private Image getImage(String fileName) {
+        return new Image(getClass().getClassLoader().getResourceAsStream(fileName)); //assuming files map to real images
+    }
+
+    protected Node getIcon() {
+        return myVBox;
+    }
+}
---
+++ src/frontend/src/ui_private/features/TurtleImagesFeature.java
@@ -0,0 +1,47 @@
+package ui_private.features;
+
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import state.StateManager;
+import state.Turtle;
+
+public class TurtleImagesFeature extends Feature {
+    private static final double V_GAP = 10;
+    private static final double H_GAP = 10;
+    private static final int COLUMNS = 3;
+
+    private int finalIconIndex;
+    private ScrollPane myScrollPane;
+    private GridPane myGridPane;
+
+    public TurtleImagesFeature(StateManager manager) {
+        super(manager);
+        initGridPane();
+        for (Turtle turtle : myStateManager.getTurtleManager().getTurtles())
+            addIcon(turtle);
+        myScrollPane = new ScrollPane(myGridPane);
+    }
+
+    private void initGridPane() {
+        myGridPane = new GridPane();
+        myGridPane.setVgap(V_GAP);
+        myGridPane.setHgap(H_GAP);
+        myStateManager.getTurtleManager().getNewTurtleProperty().addListener((o, old, neww) -> addIcon(neww));
+    }
+
+    private void addIcon(Turtle turtle) {
+        myGridPane.add(new TurtleIcon(turtle).getIcon(), finalIconIndex % COLUMNS, finalIconIndex / COLUMNS);
+        finalIconIndex ++;
+    }
+
+    @Override
+    protected Node getMainComponent() {
+        return myScrollPane;
+    }
+
+    @Override
+    protected boolean getHasHorizontalLayout() {
+        return false;
+    }
+}
