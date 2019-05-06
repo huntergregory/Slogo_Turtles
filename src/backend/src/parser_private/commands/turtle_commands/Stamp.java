2829c0ba821ec32b182f52b5ba78724a2d46c38d
David Miron <david.miron@duke.edu>
2019-05-06 00:36:44 -0400

    Implement stamp

--- src/backend/src/parser_private/commands/turtle_commands/Stamp.java
+++ src/backend/src/parser_private/commands/turtle_commands/Stamp.java
@@ -4,9 +4,12 @@ import state.ICommand;
 import state.Turtle;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stamp extends TurtleCommand {
+
+    private static int currID = -10000;
 
     public Stamp(List<ICommand> params) {
         super(params);
@@ -16,15 +19,22 @@ public class Stamp extends TurtleCommand {
     public double execute() {
         System.out.println("Stamp");
 
-        return runTurtleCommand(turtle -> {
-            for (int i = 0; i < 4; i++) {
-                moveAndTurn(turtle);
-            }
-            return (double)turtle.getID();
+        //for (int i = 0; i < 4; i++) {
+        //    moveAndTurn(turtle);
+        //}
+        runTurtleCommand(turtle -> {
+            List<Integer> ids = new ArrayList<>();
+            ids.add(currID);
+            List<Integer> sameID = new ArrayList<>();
+            sameID.add(turtle.getID());
+            myStateManager.getTurtleManager().setTurtlesActive(ids);
+            myStateManager.getTurtleManager().setTurtlesActive(sameID);
+            return 0.0;
         });
-
-
+        currID++;
+        return 0;
     }
+
     private void moveAndTurn(Turtle turtle) {
         Point2D pos = turtle.getPosition();
         turtle.setPosition(pos.getX() + Math.sin(turtle.getHeading()) * 20,
